153b1a27b706324e6a3d71c7d2516a1fd04eed3f
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-17 09:09:32 +0530

    Coding Standards
    various coding standards to follow

---
+++ CodingStandards/CodingStandards/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodingStandards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coding Standards");
+        }
+    }
+}
+
+
+/*
+
+## Part of coding Standards
+
+1. Reasonable vaiable name.
+2. Proper Method Name.
+3. Always write comments for functions explaining the functionality of that function.
+4. Single line comments: for describing variables or what an if statement is for.
+5. Multi line comment: for storing user data and connecting to the database.
+6. XML documentation comments:
+        ///<summary>
+        /// This is the main method
+        /// </summary>
+
+*/
