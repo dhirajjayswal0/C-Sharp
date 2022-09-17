e74837f20ede0ffa5aa29c614e12cd0e2d6ac080
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-17 12:22:48 +0530

    Console methods
    various console methods

---
+++ consoleMethods/consoleMethods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace consoleMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console Methods");
+        }
+    }
+}
+
+
+
+/*
+
+## console methods
+Console.Write("text here");     -> prints and keeps the cursor at the same line.
+Console.WriteLine("text here");     -> prints and move the cursor to the next line.
+Console.Read();      ->Takes single input of type string and it returns the Ascii value of that input.
+Console.ReadLine();      ->Takes a string or integer input and returns it as the output value.
+Console.ReadKey();      -> Takes a single input of type string and it returns the key info.
+
+*/
