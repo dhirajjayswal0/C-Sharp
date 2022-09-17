fb38ef2b4d63689378e4fd2390de1b0d7aab66d3
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-17 19:34:53 +0530

    Implicit and Explicit Conversions
    Implicit and Explicit Conversions

---
+++ Implicit_and_Explicit_Conversions/Implicit_and_Explicit_Conversions/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Implicit_and_Explicit_Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion
+            int num = 12390532;
+            long bigNum = num;
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+
+
+            // Explicit conversion
+            // cast double to int ;
+            double myDouble = 13.37;
+            int myInt;
+            myInt = (int)myDouble;
+
+
+
+            // typeConversion
+            string myString = myDouble.ToString(); // "13.37"
+
+            bool sunIsShining = false;
+            string myBoolString = sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString);
+            Console.ReadLine();
+        }
+    }
+}
+
