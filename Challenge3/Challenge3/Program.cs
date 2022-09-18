99c4942660ccd417117f8c672639bc3971fe7ad0
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-18 17:07:14 +0530

    Challenge3
    Challenge3

---
+++ Challenge3/Challenge3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Challenge3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textString;
+            string textChar;
+
+            Console.Write("Enter a string here: ");
+            textString = Console.ReadLine();
+
+            Console.Write("Enter the character to search: ");
+            textChar = Console.ReadLine();
+
+            int indexofString = textString.IndexOf(textChar);
+            Console.WriteLine(indexofString);
+
+
+            // Now for concatination
+
+            string firstName;
+            string LastName;
+
+            Console.Write("Enter First Name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            LastName = Console.ReadLine();
+
+            String fullName = $"{firstName} {LastName}";
+            Console.WriteLine(fullName);
+
+            Console.ReadLine();
+        }
+    }
+}
+
