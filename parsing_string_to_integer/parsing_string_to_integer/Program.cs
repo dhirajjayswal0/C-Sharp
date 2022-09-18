68904c21d40095d113421a6ce65691b90fe0775c
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-18 11:40:26 +0530

    Parsing string to integer
    Parsing string to integer

---
+++ parsing_string_to_integer/parsing_string_to_integer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace parsing_string_to_integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myString = "15";
+            string mySecondString = "13";
+
+            // String can be parse to int using Int32 method.
+            int num1 = Int32.Parse(myString);
+            int num2 = Int32.Parse(mySecondString);
+
+            int sum = num1 + num2;
+
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
+
