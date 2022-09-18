58ddecb0e96efd3193f86ae5aa30bbeadecc15db
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-18 16:50:01 +0530

    Challenge2
    Challenge2

---
+++ Challenge2/Challenge2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Challenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Please enter your name and press enter");
+            name = Console.ReadLine();
+
+            Console.WriteLine(name.ToUpper());
+            Console.WriteLine(name.ToLower());
+            Console.WriteLine(name.Trim());
+            Console.WriteLine(name.Substring(2, 3));
+            Console.ReadLine();
+        }
+    }
+}
+
--- StringMethods/StringMethods/Program.cs
+++ StringMethods/StringMethods/Program.cs
@@ -6,7 +6,39 @@ namespace StringMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+
+            ## Concat(Object, Object)	
+            Concatenates the string representations of two specified objects.
+
+
+            ## Substring(Int32) :
+            Retrieves a substring from this instance.
+            The substring starts at a specified character position and continues to the end of the string.
+
+
+            ## ToLower() : 
+            Returns a copy of this string converted to lowercase.
+
+
+            ## ToUpper()	
+            Returns a copy of this string converted to uppercase.
+
+
+            ## Trim()	
+            Removes all leading and trailing white-space characters from the current string.
+
+
+            ## IndexOf(String)	
+            Reports the zero-based index of the first occurrence of the specified string in this instance.
+
+
+            ## IsNullOrWhiteSpace(value)
+            The example displays the output as True 0r False.
+
+
+
+            */
         }
     }
 }
