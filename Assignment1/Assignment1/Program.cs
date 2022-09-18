6041fa8d6072ea75262aed3154e95572ac52bbbb
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-18 11:51:31 +0530

    Assignment1
    Assignment1

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Assignment1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float floatnum = float.Parse(stringForFloat);
+            int intnum = int.Parse(stringForInt);
+
+            Console.WriteLine(floatnum);
+            Console.WriteLine(intnum);
+            Console.ReadLine();
+        }
+    }
+}
+
