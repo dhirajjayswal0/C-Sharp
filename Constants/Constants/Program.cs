59c908c7fbafd4cceff9e9ce621fd45cd0d913a1
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-18 17:31:54 +0530

    Constants
    Constants

---
+++ Constants/Constants/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Constants
+{
+    // Constants are immutable valueswhich are known at the compile time.
+    // They do not change for the life of the program.
+
+    class Program
+    {
+        // constants as fields
+        const double PI = 3.14159265359;
+        const int weeks = 52, months = 12;
+
+        // Create a constant of type string with ypur birthday as its value
+        const string birthday = "29.03.1998";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PI);
+            Console.WriteLine(weeks);
+            Console.WriteLine(birthday);
+            Console.ReadLine();
+        }
+    }
+}
+
