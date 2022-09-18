72d99a628c418c0842c0c67f955925df06638758
dhiraj Jayswal <dhirajjayswal0@gmail.com>
2022-09-18 16:07:10 +0530

    String Manipulation
    String Manipulation

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Dhiraj";
+            int age = 25;
+
+            // 1. String Concatination
+            Console.WriteLine("String Concatination");
+            Console.WriteLine("Hello my name is " + name + ", I am " + age + " years old.");
+
+
+            // 2. String formatting
+            Console.WriteLine("\nString formatting");
+            Console.WriteLine("Hello my name is {0}, I am {1} years old.", name, age);
+
+
+            // 3. String interpolation
+            Console.WriteLine("\nString interpolation");
+            Console.WriteLine($"Hello my name is {name} , I am {age} years old.");
+
+
+            // 4. Verbatim string
+            // Here it will take everything inside the string literally.
+            Console.WriteLine("\nVerbatim string");
+            Console.WriteLine(@"Hello my name is {name} , I am {age} years old.");
+
+            Console.ReadLine();
+        }
+    }
+}
+
